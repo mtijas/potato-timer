5cf8a21d20c745f7b8de92f39b163943836fe7ad
Markus Ijäs <markus.ijas@outlook.com>
2021-02-08 21:21:33 +0200

    First basically working version with working ui

--- src/TimerEngine.py
+++ src/TimerEngine.py
@@ -2,25 +2,39 @@ import time
 
 class TimerEngine:
     def __init__(self):
+        self.alarm = False
+        self.work_count = 0
+        self.short_count = 0
+        self.long_count = 0
         self.current_timer_id = 0
         self.previous_monotonic = 0
         self.running = False
+        self.started_at = None
         self.timers = [
-                ("work", 20),
-                ("pause", 5),
-                ("work", 20),
-                ("pause", 5),
-                ("work", 20),
-                ("long pause", 15),
+                ("work", 2),
+                ("short break", 1),
+                ("work", 2),
+                ("short break", 1),
+                ("work", 2),
+                ("short break", 1),
+                ("work", 2),
+                ("long break", 3),
         ]
         self.timer_name = None
         self.timer_duration = 0
         self.time_elapsed = 0
+        self.total_time_elapsed = 0
+        self.total_time_working = 0
         self.select_timer(0)
+
+    def ack_alarm(self):
+        self.alarm = False
 
     def start_timer(self):
         self.running = True
         self.previous_monotonic = time.monotonic()
+        if self.started_at == None:
+            self.started_at = time.localtime()
 
     def stop_timer(self):
         self.running = False
@@ -29,8 +43,10 @@ class TimerEngine:
         if self.running:
             self.time_elapsed += self.get_elapsed_time()
             if self.time_elapsed >= self.timer_duration:
-                self.time_elapsed = 0
+                self.alarm = True
+                self.increase_counts()
                 self.next_timer()
+                self.time_elapsed = 0
 
     def get_elapsed_time(self):
         current_m = time.monotonic()
@@ -39,14 +55,30 @@ class TimerEngine:
         return elapsed
 
     def next_timer(self):
+        self.calc_stats()
         self.current_timer_id += 1
         if self.current_timer_id >= len(self.timers):
             self.current_timer_id = 0
         self.select_timer(self.current_timer_id)
 
+    def increase_counts(self):
+        if self.timer_name == "work":
+            self.work_count += 1
+        elif self.timer_name == "short break":
+            self.short_count += 1
+        elif self.timer_name == "long break":
+            self.long_count += 1
+
+    def calc_stats(self):
+        if self.timer_name == "work":
+            self.total_time_working += self.time_elapsed
+        self.total_time_elapsed += self.time_elapsed
+
     def select_timer(self, timer_id):
         if timer_id < len(self.timers):
-            self.timer_name, self.timer_duration = self.timers[timer_id]
+            name, duration = self.timers[timer_id]
+            self.timer_name = name
+            self.timer_duration = duration*60
         else:
             raise IndexError("Timer not found")
 
