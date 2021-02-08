710afe207947aedb352c613a34f9687bd2e4f44d
Markus Ijäs <markus.ijas@outlook.com>
2021-02-08 13:12:56 +0200

    Timer is somewhat running and UI kind of working

--- src/TimerEngine.py
+++ src/TimerEngine.py
@@ -1,6 +1,31 @@
 class TimerEngine:
     def __init__(self):
         self.timers = [("work", 20), ("pause", 5)]
+        self.running = False
+        self.current_timer_id = 0
+        self.time_elapsed = 0
+        self.timer_duration = 0
 
-    def getTimers(self):
+
+
+    def get_timers(self):
         return self.timers
+
+
+
+    def start_timer(self):
+        self.running = True
+
+
+
+    def stop_timer(self):
+        self.running = False
+
+
+
+    def update(self):
+        if self.running:
+            self.time_elapsed += 1
+        if self.time_elapsed >= 5:
+            self.time_elapsed = 0
+            self.stop_timer()
